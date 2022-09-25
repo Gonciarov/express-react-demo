cbc919f769423df90f747542d04f81c5ada1de54
Gonciarov <gonciarov@yandex.ru>
2022-09-25 14:24:59 +0100

    http requests to backend work good

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser'); 
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api', (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
+app.post('/api', (req, res) => {
+    let data = req.body.data
+    console.log(data)
 })
 
 app.listen(5000, () => console.log("Running on 5000"))
